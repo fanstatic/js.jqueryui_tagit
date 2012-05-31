711e9af31ddf553138487f18c0a14542f594f83f
Andreas kaiser <disko@binary-punks.com>
2012-05-31 16:03:46 +0200

    prepare release

--- setup.py
+++ setup.py
@@ -1,14 +1,11 @@
 from setuptools import setup, find_packages
 import os
 
-# The version of the wrapped library is the starting point for the
-# version number of the python package.
-# In bugfix releases of the python package, add a '-' suffix and an
-# incrementing integer.
-# For example, a packaging bugfix release version 1.4.4 of the
-# js.jquery package would be version 1.4.4-1 .
+# The version of the wrapped library is the starting point for the version number of the python package.
+# In bugfix releases of the python package, add a '-' suffix and an incrementing integer.
+# For example, a packaging bugfix release version 1.4.4 of the js.jquery package would be version 1.4.4-1 .
 
-version = '2.0.24dev'
+version = '2.0.24'
 
 
 def read(*rnames):
